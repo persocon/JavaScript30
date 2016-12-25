a7b0f7ee6ad3d41e9bd9711077e793531e7524a4
Pedro Costa Neves <neo.nyow@gmail.com>
2016-12-25 14:22:32 -0200

    29

--- 29 - Countown Timer/scripts.js
+++ 29 - Countown Timer/scripts.js
@@ -1,33 +1,29 @@
-let countdown;
+let countDown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  // clear any existing timers
-  clearInterval(countdown);
-
+  clearInterval(countDown);
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
   displayEndTime(then);
 
-  countdown = setInterval(() => {
+  countDown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
-    if(secondsLeft < 0) {
-      clearInterval(countdown);
+    if (secondsLeft < 0) {
+      clearInterval(countDown);
       return;
     }
-    // display it
     displayTimeLeft(secondsLeft);
-  }, 1000);
+  }, 1000)
 }
 
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}:${remainderSeconds < 10 ? '0' : '' }${remainderSeconds}`;
+  const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
   document.title = display;
   timerDisplay.textContent = display;
 }
@@ -35,9 +31,9 @@ function displayTimeLeft(seconds) {
 function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
-  const adjustedHour = hour > 12 ? hour - 12 : hour;
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
+  const adjustedHour = hour > 12 ? hour - 12 : hour;
+  endTime.textContent = `Be Back at ${adjustedHour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }
 
 function startTimer() {
@@ -46,10 +42,9 @@ function startTimer() {
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));
-document.customForm.addEventListener('submit', function(e) {
-  e.preventDefault();
+document.customForm.addEventListener('submit', function(event){
+  event.preventDefault();
   const mins = this.minutes.value;
-  console.log(mins);
   timer(mins * 60);
   this.reset();
 });
